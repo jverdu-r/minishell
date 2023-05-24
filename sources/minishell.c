cc3963683407b972a854deddb375efdea6eaee8d
Jorge Verdu ruiz <jverdu-r@c1r10s6.42madrid.com>
2023-05-24 17:35:01 +0200

    program now will work only with no arguments

--- sources/minishell.c
+++ sources/minishell.c
@@ -6,19 +6,26 @@
 /*   By: jverdu-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 15:15:26 by jverdu-r          #+#    #+#             */
-/*   Updated: 2023/05/24 15:33:19 by jverdu-r         ###   ########.fr       */
+/*   Updated: 2023/05/24 17:32:40 by jverdu-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
 	char	*input;
+	char	*err;
 	int		loop;
 
+	err = argv[1];
 	rl_on_new_line();
 	loop = 1;
+	if (argc > 1)
+	{
+		printf("ERROR too many arguments"); 
+		return (1);
+	}
 	while (loop != 0)
 	{
 		input = readline("minishel>");
