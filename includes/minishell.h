c3647ff7afc6e3ed84f6350c659d99dc1c90a452
Jorge Verdu ruiz <jverdu-r@c1r12s4.42madrid.com>
2023-05-25 13:22:43 +0200

    starting parsing operations

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jverdu-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 15:07:55 by jverdu-r          #+#    #+#             */
-/*   Updated: 2023/05/24 15:11:47 by jverdu-r         ###   ########.fr       */
+/*   Updated: 2023/05/25 13:21:18 by jverdu-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,35 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-int	ft_strcmp(const char *s1, const char *s2); //function located in test_functions.c should be removed
+typedef enum	s_token
+{
+	PIPE = 1,
+	LESS,
+	LESS_LESS,
+	GREAT,
+	GREAT_GREAT,
+}	t_token;
+
+typedef struct	s_lexer
+{
+	char			*word;
+	t_token			token;
+	int				index;
+	struct s_lexer	*next;
+	struct s_lexer	*prev;
+}	t_lexer;
+
+int		lexer_length(t_pile *list);
+t_lexer	*lexer_new(void *content);
+t_lexer	*lexer_last(t_lexer *lst);
+void	lexer_add(t_lexer **head, t_list *new);
+void	lexer_addback(t_lexer **head, t_list *new);
+void	lexer_delone(t_lexer **lst);
+void	get_cmds(const char **input);
+
+//functions of libft located in sources/test_functions.c
+//must be removed when not necesary
+int		ft_strcmp(const char *s1, const char *s2);
+char	**ft_split(char const *str, char c);
 
 #endif
---
+++ sources/lexer_lst.c
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer_lst.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jverdu-r <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/25 11:45:56 by jverdu-r          #+#    #+#             */
+/*   Updated: 2023/05/25 13:21:16 by jverdu-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+int	lexer_length(t_lexer *list)
+{
+	int		i;
+	t_lexer	*aux;
+
+	if (!list)
+		return (0);
+	aux = list;
+	i = 0;
+	while (aux)
+	{
+		aux = aux->next;
+		i++;
+	}
+	return (i);
+}
+
+t_lexer	*lexer_new(char *word, t_token token)
+{
+	t_lexer	*new;
+
+	new = malloc(sizeof(t_lexer));
+	if (!new)
+		return (NULL);
+	if (word)
+		new->word = word;
+	if (token)
+		new.token = token;
+	new.index = 0;
+	new->next = NULL;
+	new->prev = NULL;
+	return (new);
+}
+
+t_lexer *lexer_last(t_lexer *lst)
+{
+	t_lexer	*tmp;
+
+	if (!lst)
+		return (NULL);
+	tmp = lst;
+	while (tmp->next != NULL)
+		tmp = tmp = tmp->next;
+	return (tmp);
+}
+
+void	lexer_del(t_lexer **lst)
+{
+	if (lst && *lst)
+	{
+		if (lst_length(*lst) > 1)
+		{
+			lst[0] = lst[0]->next;
+			free(lst[0]->prev);
+			lst[0]->prev = NULL;
+		}
+		else
+		{
+			free(lst[0]);
+			lst[0] = NULL;
+		}
+	}
+}
+
+void	lexer_add(t_lexer **head, t_lexer *new)
+{
+	t_lexer	*tmp;
+
+	tmp = *head;
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		new->next = tmp;
+		tmp-prev = new;
+		tmp = new;
+		*head = tmp;
+	}
+}
+
+void	lexer_add_back(t_lexer **head, t_lexer *new)
+{
+	t_lexer	*tmp;
+
+	tmp = *head;
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		while (tmp)
+		{
+			if (!tmp->next)
+			{
+				tmp->next = new;
+				new->prev = tmp;
+				tmp = new;
+			}
+			tmp = tmp->next;
+		}
+	}
+}
